5b822c5f4bd44585c45134437bfa1f28a953af22
Syl D <sdelescluse@atecna.fr>
2023-06-13 16:15:02 +0200

    test: HelloWorld component

---
+++ test/hello-world.test.ts
@@ -0,0 +1,37 @@
+import { beforeAll, beforeEach, expect, test } from 'vitest'
+import type { DOMWrapper, VueWrapper } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+let wrapper: VueWrapper
+let buttonPlusElement: DOMWrapper<HTMLButtonElement>
+let buttonMinusElement: DOMWrapper<HTMLButtonElement>
+let divElement: DOMWrapper<HTMLDivElement>
+
+beforeAll(() => {
+  wrapper = mount(HelloWorld)
+})
+
+beforeEach(() => {
+  buttonPlusElement = wrapper.findAll('button').at(0)!
+  buttonMinusElement = wrapper.findAll('button').at(1)!
+})
+
+test('Au départ le compteur est à zéro', () => {
+  divElement = wrapper.find('.card div')
+  expect(divElement.text()).toBe('count is 0')
+  expect(wrapper.vm.count).toBe(0)
+})
+
+test('Cliquer sur le bouton + ajoute 1 au compteur', async () => {
+  await buttonPlusElement.trigger('click')
+  divElement = wrapper.find('.card div')
+  expect(divElement.text()).toBe('count is 1')
+})
+
+test('Cliquer sur le bouton - retire 1 au compteur', async () => {
+  wrapper.vm.count = 3
+  await buttonMinusElement.trigger('click')
+  divElement = wrapper.find('.card div')
+  expect(divElement.text()).toBe('count is 2')
+})
