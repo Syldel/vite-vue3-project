e9ee859e77ce4533074ce61935b252053c1b5eab
Syl D <sdelescluse@atecna.fr>
2023-06-28 10:49:46 +0200

    feat(components): loading spin

---
+++ src/components/LoadingSpin.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    default: 'Loading...',
+  },
+})
+</script>
+
+<template>
+  <div class="loading-spin">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+      <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+    </svg>
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.loading-spin
+  display: flex
+  align-items: center
+  gap: 5px
+
+  svg
+    width: 1.25rem
+    height: 1.25rem
+    color: #3B82F6
+    animation: spin 1s linear infinite
+    @keyframes spin
+      from
+        transform: rotate(0deg)
+      to
+        transform: rotate(360deg)
+
+    circle
+      opacity: .25
+    path
+      opacity: .75
+</style>
