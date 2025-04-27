c9f0bc7b456f96aa4e1e027fd224ec973f666d26
Syldel <nightfand@hotmail.com>
2025-04-27 23:36:38 +0200

    feat(router): enable hash mode routing with createWebHashHistory()

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 export const RouteNames = {
   HOME: 'home',
@@ -49,6 +49,6 @@ export const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
