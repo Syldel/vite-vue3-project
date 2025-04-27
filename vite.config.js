5f932f980b0230bdb5c26c6feb7bbc6c6122f451
Syldel <nightfand@hotmail.com>
2025-04-27 22:46:26 +0200

    build(vite): load environment variables using loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,38 @@
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [],
-    }),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'vitest',
-        '@vueuse/core',
-      ],
-      dts: true,
-    }),
-  ],
-  test: {
-    include: ['**/*.test.{j,t}s'],
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '.', 'src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [],
+      }),
+      AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+          'vitest',
+          '@vueuse/core',
+        ],
+        dts: true,
+      }),
+    ],
+    test: {
+      include: ['**/*.test.{j,t}s'],
+      environment: 'jsdom',
     },
-  },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, '.', 'src'),
+      },
+    },
+  }
 })
